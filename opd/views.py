794e13c2984191a7be8ec2c5a03a3b8a1530ae8e
ujjawal <ujjawal17032002@gmail.com>
2024-09-28 03:44:41 +0530

    minor changes

--- opd/views.py
+++ opd/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 import json
 from django.http import HttpResponseForbidden
 from django.http.response import HttpResponseBadRequest
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from opd.forms import (
@@ -277,7 +277,6 @@ def medicine(request, id):
 @user_passes_test(is_doctor, login_url="home:home_page")
 def add_product(request):
     if request.method == "POST":
-        print(request.POST)
         form = InventoryItemsForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data["name"]
